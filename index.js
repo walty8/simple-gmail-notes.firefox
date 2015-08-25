f68edcfb6d646dc0c05997c7d133f603171a8273
Walty Yeung <walty8@gmail.com>
2015-08-25 14:43:55 +0000

    web auth ready, able to collect refresh token now.

--- index.js
+++ index.js
@@ -2,8 +2,33 @@ console.log("walty test@1@index.js");
 
 var Request = require("sdk/request").Request;
 var OAuthConsumer = require("oauthorizer/lib/oauthconsumer.js").OAuthConsumer;
+var ss = require("sdk/simple-storage");
+var self = require("sdk/self");
+var pageMod = require("sdk/page-mod");
 
 
+//The refresh token, access token and email for google drive are stored in
+//local storage. Different gmails may have different sets of storage.
+function setStorage(email, key, value) 
+{
+  var storageKey = email + "||" + key;
+  //localStorage[storageKey] = value;
+  ss.storage[storageKey] = value;
+}
+
+function getStorage(email, key)
+{
+  if(!email || email.indexOf("@") < 0){
+    console.log("email not found!");
+  }
+
+  var storageKey = email + "||" + key;
+  //value = localStorage[storageKey];
+  value = ss.storage[storageKey];
+
+  console.log("@20, gets storage", email, key, value);
+  return value;
+}
 console.log("@2, OAuthConsumer", OAuthConsumer);
 
 /*
@@ -26,8 +51,6 @@ var walty_test = 123;
 
 
 
-var self = require("sdk/self");
-var pageMod = require("sdk/page-mod");
 
 pageMod.PageMod({
   include: ["https://mail.google.com/*", "http://mail.google.com/*"],
@@ -53,8 +76,41 @@ function initialize(sender, messageId){
 
 }
 
-function loginGoogleDrive(sender, messageId)
-{
+function updateRefreshTokenFromCode(sender, messageId){
+  var email = sender.email;
+  sendAjax({
+    type: "POST",
+    contentType: "application/x-www-form-urlencoded",
+    data: {
+        "code":getStorage(sender.email, "code"),
+        "client_id":"38131814991-p4u809qrr5ee1bsehregd4os69jf2n7i.apps.googleusercontent.com",
+        "client_secret":"mdA0U_jSkAjI_1x8pdgtrx02",
+        "redirect_uri":"http://localhost",
+        "grant_type":"authorization_code"
+    },
+    url: "https://www.googleapis.com/oauth2/v3/token",
+    error: function(data){
+      showRefreshTokenError(sender, JSON.stringify(data));  //walty temp
+    },
+    success: function(data){
+      if(!data.refresh_token){
+        showRefreshTokenError(sender, "Google Drive token could not be collected.");
+        setStorage(email, "access_token", data.access_token); //for future revoking
+      }
+      else{
+        console.log("@59, success", data);
+        console.log(data);
+        setStorage(email, "refresh_token", data.refresh_token);
+        setStorage(email, "access_token", data.access_token);
+        //initialize(sender, messageId);
+        //updateUserInfo(email);
+    //sendMessage({action:"show_log_out_prompt"});
+    //sendMessage({action:"enable_edit"});
+      }
+    }
+  });
+}
+function loginGoogleDrive(sender, messageId) {
 
     var calls = {
       userAuthorizationURL: "https://accounts.google.com/o/oauth2/auth"
@@ -78,10 +134,20 @@ function loginGoogleDrive(sender, messageId)
       'scope': 'https://www.googleapis.com/auth/drive.file' // contacts
     };
 
+    p.useInternalStorage = false;
+
 
     function testCallback(svc) {
       console.log("@59", svc);
       console.log("@60", svc.token);
+
+      var code = svc.token
+      code = code.replace(/[#]/g, "");
+      console.log("@53:" + code);
+      setStorage(sender.email, "code", code);
+      updateRefreshTokenFromCode(sender, messageId);
+
+      //var code = code.replace(/[#]/g, "");
       // your access_token is svc.token
     }
     console.log("@70, walty test");
@@ -121,8 +187,48 @@ function sendUrl(worker) {
   });
 }
 
-function sendMessage(sender, message)
-{
+function sendMessage(sender, message) {
   console.log("@116", sender, message);
   sender.worker.port.emit("SGN_background", message); 
+}
+
+function showRefreshTokenError(sender, message) {
+  console.log("@196, token error", message);
+}
+
+function sendAjax(ajaxConfig) {
+  var request = Request({
+    url: ajaxConfig.url,
+    contentType: ajaxConfig.contentType,
+    content: ajaxConfig.data,
+    onComplete: function(response){
+      if(response.status >= 400){ //got error
+        if(ajaxConfig.error){
+          console.log("@205, ajax error", response.status, ajaxConfig.url, response.text);
+          ajaxConfig.error(response.text);
+        }
+      }
+      else{ //success
+        if(ajaxConfig.success){
+          console.log("@211, ajax success", ajaxConfig.url, response.text);
+          ajaxConfig.success(response.text);
+        }
+      }
+
+    }
+      
+  });
+
+  switch(ajaxConfig.type){
+    case "POST":
+      request.post();
+      break;
+    case "PUT":
+      request.put();
+    case "DELETE":
+      request.delete();
+    default:
+      request.get();
+  }
+
 }
