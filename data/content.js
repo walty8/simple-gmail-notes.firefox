540579008491fd863f95783e6d6322434de1510f
walty8 <walty8@gmail.com>
2015-09-14 22:21:23 +0800

    updated file structure

--- data/content.js
+++ data/content.js
@@ -211,11 +211,11 @@ function setupPage(dataurl){
   // Handle the message
   if(top.document == document) {
      var j = document.createElement('script');
-     j.src = dataurl+'/jquery-1.10.2.min.js';
+     j.src = dataurl+'/lib/jquery-1.10.2.min.js';
      (document.head || document.documentElement).appendChild(j);
 
      var g = document.createElement('script');
-     g.src = dataurl+'/gmail.js';
+     g.src = dataurl+'/lib/gmail.js';
      (document.head || document.documentElement).appendChild(g);
 
      var s = document.createElement('script');
